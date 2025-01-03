dd10811496b35a1bde0693238e533a361cf4b91b
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2025-01-03 17:35:57 +0700

    update junit 5 platform for vsc

--- Lab1/ContactManagerApplication/src/ContactManagerTest.java
+++ Lab1/ContactManagerApplication/src/ContactManagerTest.java
@@ -1,15 +1,38 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
-
-class ContactManagerTest {
+public class ContactManagerTest {
     @Test
-    // @DisplayName("Should Create Contact")
     public void shouldCreateContact() {
         ContactManager contactManager = new ContactManager();
         contactManager.addContact("John", "Doe", "0123456789");
         assertFalse(contactManager.getAllContacts().isEmpty());
         assertEquals(1, contactManager.getAllContacts().size());
     }
+
+    @Test
+    public void shouldThrowRuntimeExceptionWhenFirstNameIsNull() {
+        ContactManager contactManager = new ContactManager();
+        assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact(null, "Doe", "0123456789");
+        });
+    }
+
+    @Test
+    public void shouldThrowRuntimeExceptionWhenLastNameIsNull() {
+        ContactManager contactManager = new ContactManager();
+        assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("John", null, "0123456789");
+        });
+    }
+
+    @Test
+    public void shouldThrowRuntimeExceptionWhenPhoneNumberIsNull() {
+        ContactManager contactManager = new ContactManager();
+        assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("John", "Doe", null);
+        });
+    }
+
+    
 }
