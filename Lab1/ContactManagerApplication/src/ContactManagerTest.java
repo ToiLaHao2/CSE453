d362ba7e0e3ccd0f4634bf4713377c1087277eaa
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2025-01-03 17:52:56 +0700

    update

--- Lab1/ContactManagerApplication/src/ContactManagerTest.java
+++ Lab1/ContactManagerApplication/src/ContactManagerTest.java
@@ -1,8 +1,15 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
 public class ContactManagerTest {
     @Test
+    @DisplayName("Should Create Contact")
     public void shouldCreateContact() {
         ContactManager contactManager = new ContactManager();
         contactManager.addContact("John", "Doe", "0123456789");
@@ -11,6 +18,7 @@ public class ContactManagerTest {
     }
 
     @Test
+    @DisplayName("Should Not Create Contact When First Name is Null")
     public void shouldThrowRuntimeExceptionWhenFirstNameIsNull() {
         ContactManager contactManager = new ContactManager();
         assertThrows(RuntimeException.class, () -> {
@@ -19,6 +27,7 @@ public class ContactManagerTest {
     }
 
     @Test
+    @DisplayName("Should Not Create Contact When Last Name is Null")
     public void shouldThrowRuntimeExceptionWhenLastNameIsNull() {
         ContactManager contactManager = new ContactManager();
         assertThrows(RuntimeException.class, () -> {
@@ -27,6 +36,7 @@ public class ContactManagerTest {
     }
 
     @Test
+    @DisplayName("Should Not Create Contact When Phone Number is Null")
     public void shouldThrowRuntimeExceptionWhenPhoneNumberIsNull() {
         ContactManager contactManager = new ContactManager();
         assertThrows(RuntimeException.class, () -> {
@@ -34,5 +44,24 @@ public class ContactManagerTest {
         });
     }
 
-    
+    @BeforeAll
+    public static void setupAll() {
+        System.out.println("Should Print Before All Tests");
+    }
+
+    @BeforeEach
+    public void setup() {
+        System.out.println("Instantiating Contact Manager");
+        contactManager = new ContactManager();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Should Execute After Each Test");
+    }
+
+    @AfterAll
+    public static void tearDownAll() {
+        System.out.println("Should be executed at the end of the Test");
+    }
 }
