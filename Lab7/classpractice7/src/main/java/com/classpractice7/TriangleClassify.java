71b71c87384f7f88fac4b05e07914aed829a5ba1
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2025-03-03 21:09:42 +0700

    update

--- Lab7/classpractice7/src/main/java/com/classpractice7/TriangleClassify.java
+++ Lab7/classpractice7/src/main/java/com/classpractice7/TriangleClassify.java
@@ -1,8 +1,9 @@
 package com.classpractice7;
 
 public class TriangleClassify {
-	private int side1, side2, side3;
-	private String TriangleType;
+	private int side1;
+	private int side2;
+	private int side3;
 
 	public void setSide(int s1, int s2, int s3) {
 		this.side1 = s1;
@@ -32,25 +33,14 @@ public class TriangleClassify {
 	}
 
 	public String classify() {
-		boolean c1, c2, c3;
-
-		c1 = (1 <= side1) && (side1 <= 200);
-		c2 = (1 <= side2) && (side2 <= 200);
-		c3 = (1 <= side3) && (side3 <= 200);
-
-		if (!c1 || !c2 || !c3)
-			TriangleType = "OUT_OF_RANGE";
-		else {
-			if (isTriangle()) {
-				if ((side1 == side2) && (side2 == side3))
-					TriangleType = "EQUILATERAL";
-				else if ((side1 != side2) && (side1 != side3) && (side2 != side3))
-					TriangleType = "SCALENE";
-				else
-					TriangleType = "ISOSCELES";
-			} else
-				TriangleType = "IsNotTriangle";
-		}
-		return TriangleType;
+		if (side1 < 1 || side1 > 200 || side2 < 1 || side2 > 200 || side3 < 1 || side3 > 200)
+			return "OUT_OF_RANGE";
+		if (!isTriangle())
+			return "IsNotTriangle";
+		if (side1 == side2 && side2 == side3)
+			return "EQUILATERAL";
+		if (side1 != side2 && side1 != side3 && side2 != side3)
+			return "SCALENE";
+		return "ISOSCELES";
 	}
 }
